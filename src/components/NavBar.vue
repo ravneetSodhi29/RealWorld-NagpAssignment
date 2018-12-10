7f44420f1e735de1423d6cd86efc7fb9264414f5
Ravneet <ravneet.geminigal@gmail.com>
2018-12-10 22:10:05 +0530

    Basic code and routes set up

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">conduit</a>
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href>
+            <i class="ion-compose"></i>&nbsp;New Post
+          </a>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/settings">Settings</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/login">Sign in</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/register">Sign up</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
